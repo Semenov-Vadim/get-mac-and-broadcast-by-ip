7edd9c78fbc422fca380d5732e0c7d007d88fc37
Semenov-Vadim <vadymsemenov3@gmail.com>
2021-10-07 17:50:20 +0300

    style fix, minor changes

--- main.py
+++ main.py
@@ -1,47 +1,48 @@
-import netifaces    #for get_mac_and_broadcast_by_interface
-import os           #for get_inerface_by_ip()
+import netifaces  # for get_mac_and_broadcast_by_interface
+import os  # for get_inerface_by_ip()
 
 
-def get_mac_and_broadcast_by_interface(iface: str):
-    '''
+def get_mac_and_broadcast_by_interface(interface: str) -> list or None:
+    """
     Find mac and broadcast using netifaces
-    :param iface: The interface to search with
+    :param interf: The interface to search with
     :return: mac and broadcast if found, None if not found
-    '''
+    """
     for ifaces in netifaces.interfaces():
-        if ifaces == iface:
-            iface_details = netifaces.ifaddresses(iface)
+        if ifaces == interface:
+            iface_details = netifaces.ifaddresses(interface)
             for key_iface in iface_details:
-                if key_iface == netifaces.AF_LINK:  # AF_LINK means the link layer interface, e.g. Ethernet
+                # AF_LINK means the link layer interface, e.g. Ethernet
+                if key_iface == netifaces.AF_LINK:
                     return iface_details[netifaces.AF_LINK]
             else:
-                print('AF_LINK not found')
+                print("AF_LINK not found")
     else:
-        print('interface not found')
+        print("interface not found")
     return None
 
 
-def get_inerface_by_ip(ip: str):
-    '''
+def get_inerface_by_ip(ip: str) -> str or None:
+    """
     Find the interface using os library
     :param ip: The IP address to search with
     :return: The interface name if found, None if not found
-    '''
-    res = os.popen('ip route get ' + ip).read().split()
+    """
+    res = os.popen("ip route get " + ip).read().split()
 
     counter = -1
     for i in res:
         counter += 1
-        if i == 'dev':  #dev indicates the output device name
+        if i == "dev":  # dev indicates the output device name
             return res[counter + 1]
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    ip = '216.58.215.110'
+    ip = "216.58.215.110"
 
     interface = get_inerface_by_ip(ip)
     res = get_mac_and_broadcast_by_interface(interface)
 
     print(res)
-
-
